8a75babcd8e7bbb18117e7428c4159d5e7b2cf5c
Mewax07 <mewax07@gmail.com>
2025-11-07 22:43:19 +0100

    (Remove comment part)

--- typescript/pawnote/src/api/function/index.ts
+++ typescript/pawnote/src/api/function/index.ts
@@ -38,38 +38,3 @@ export class FonctionParametres extends RequestFunction<FonctionParametresReques
 		return this.decoder.decode(response);
 	}
 }
-
-/*
-export class FonctionParametres extends RequestFunction<FonctionParametresRequest> {
-	private static readonly name = "FonctionParametres";
-
-	private readonly decoder = new ResponseFunction(this.session, FonctionParametresModel, FonctionParametresSignature);
-
-	public constructor(session: Session) {
-		super(session, FonctionParametres.name);
-	}
-
-	private readonly iv = randomBytes(16);
-
-	private generateUUID(): string {
-		let iv = this.iv;
-
-		if (this.session.rsa.custom || (!this.session.rsa.custom && this.session.homepage.http)) {
-			iv = PKCS1_KEM.encrypt(this.session.rsa.publicKey, this.iv);
-		}
-
-		return base64.encode(iv);
-	}
-
-	public async send(navigatorIdentifier: string | null = null): Promise<FonctionParametresResponse> {
-		const response = await this.execute({
-			identifiantNav: navigatorIdentifier,
-			Uuid: this.generateUUID(),
-		});
-
-		this.session.aes.iv = this.iv;
-
-		return this.decoder.decode(response);
-	}
-}
-*/
--- typescript/pawnote/src/api/user_setting/index.ts
+++ typescript/pawnote/src/api/user_setting/index.ts
@@ -21,25 +21,3 @@ export class UserSetting extends RequestFunction<UserSettingRequest> {
 		return this.decoder.decode(response);
 	}
 }
-
-/*
-export class UserSetting extends RequestFunction<UserSettingRequest> {
-	private static readonly name = "ParametresUtilisateur";
-
-	private readonly decoder = new ResponseFunction(this.session, UserSettingModel, UserSettingSignature);
-
-	public constructor(session: Session) {
-		super(session, UserSetting.name);
-	}
-
-	public async send(
-		//withInvalidPassword?: boolean
-	): Promise<UserSettingResponse> {
-		const response = await this.execute({
-			// motDePasseInvalide: withInvalidPassword, // TODO: This is a stupid feature
-		});
-
-		return this.decoder.decode(response);
-	}
-}
-*/
